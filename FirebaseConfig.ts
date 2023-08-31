5cbcfa382dd1a50b5f5ea4ef8426e1831eb75234
jovimoura <joaovictors.mouraa@gmail.com>
2023-08-31 16:45:56 -0300

    envs

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getReactNativePersistence } from "./getReactNativePersistence";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFMEqcHcBUV_Ox4WqYTJf62IGEjatsM1c",
-  authDomain: "marvel-33fd2.firebaseapp.com",
-  projectId: "marvel-33fd2",
-  storageBucket: "marvel-33fd2.appspot.com",
-  messagingSenderId: "655349146543",
-  appId: "1:655349146543:web:9065b63ee8d0f55317055a",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID as string,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
--- README.md
+++ README.md
@@ -8,6 +8,30 @@ Consiste em um App feito com React Native e Firebase
 
 ## Como rodar
 
+## Você vai precisar configurar o seu firebase e suas .envs:
+
+````
+EXPO_PUBLIC_FIREBASE_API_KEY=
+EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN=
+EXPO_PUBLIC_FIREBASE_PROJECT_ID=
+EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET=
+EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=
+EXPO_PUBLIC_FIREBASE_APP_ID=
+````
+
+
+````
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID as string,
+};
+````
+
 ### Clone o proejto
 
 ```
